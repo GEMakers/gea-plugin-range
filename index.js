4b9eca9625e6675fd8c336681999cbf8769d7939
Christopher M. Baker <christopher.baker2@ge.com>
2013-12-13 16:56:42 -0500

    Added commands to range.

--- index.js
+++ index.js
@@ -144,6 +144,138 @@ function Range (appliance, base) {
     appliance.upperOven = new Oven(appliance, UPPER_OVEN_BASE);
     appliance.lowerOven = new Oven(appliance, LOWER_OVEN_BASE);
     
+    appliance.fctMode = appliance.command({
+        command: 0xa0,
+        readData: [ 0xff ],
+        format: "UInt8"
+    });
+    
+    appliance.doorLock = appliance.command({
+        command: 0xa4,
+        format: [
+            "upperOvenDoorLock:UInt8",
+            "lowerOvenDoorLock:UInt8",
+        ]
+    });
+    
+    appliance.resetEEPROM = function() {
+        appliance.send(0xa5, []);
+    };
+    
+    appliance.elementStatus = appliance.command({
+        writeCommand: 0xa6,
+        readCommand: 0xa7,
+        format: [
+            "upperOvenElementStatus:UInt8",
+            "lowerOvenElementStatus:UInt8",
+        ]
+    });
+    
+    appliance.convectionFan = appliance.command({
+        writeCommand: 0xa8,
+        readCommand: 0xa9,
+        format: [
+            "upperOvenConvectionFanDrivePercentage:UInt8",
+            "upperOvenConvectionFanRotation:UInt8",
+            "lowerOvenConvectionFanDrivePercentage:UInt8",
+            "lowerOvenConvectionFanRotation:UInt8",
+        ]
+    });
+    
+    appliance.coolingFan = appliance.command({
+        writeCommand: 0xaa,
+        readCommand: 0xab,
+        format: [
+            "upperOvenCoolingFan:UInt8",
+            "lowerOvenCoolingFan:UInt8",
+        ]
+    });
+    
+    appliance.coolingFanRevolutionsPerMinute = appliance.command({
+        command: 0xac,
+        endian: "big",
+        format: [
+            "upperOvenCoolingFanRevolutionsPerMinute:UInt16",
+            "lowerOvenCoolingFanRevolutionsPerMinute:UInt16",
+        ]
+    });
+    
+    appliance.mainControlModuleStatus = appliance.command({
+        command: 0xae,
+        format: [
+            "upperOvenCookMode:UInt8",
+            "upperOvenErrors:Bytes@3",
+            "lowerOvenCookMode:UInt8",
+            "lowerOvenErrors:Bytes@3"
+        ]
+    });
+    
+    appliance.analogInputs = appliance.command({
+        command: 0xaf,
+        endian: "big",
+        format: [
+            "upperOvenRtdFine:UInt16",
+            "upperOvenRtd:UInt16",
+            "upperOvenProbe:UInt16",
+            "lowerOvenRtdFine:UInt16",
+            "lowerOvenRtd:UInt16",
+            "lowerOvenProbe:UInt16"
+        ]
+    });
+    
+    appliance.inputStatus = appliance.command({
+        command: 0xb1,
+        endian: "big",
+        format: [
+            "upperOvenInputStatus:UInt16",
+            "lowerOvenInputStatus:UInt16"
+        ]
+    });
+    
+    appliance.keysCurrentlyPressed = appliance.command({
+        command: 0xb3,
+        readData: [0],
+        format: [
+            "keyStatus:UInt16",
+            "keyBitmap:Bytes@5"
+        ]
+    });
+    
+    appliance.latchedKeyPresses = appliance.command({
+        command: 0xb3,
+        readData: [1],
+        format: [
+            "keyStatus:UInt16",
+            "keyBitmap:Bytes@5"
+        ]
+    });
+    
+    appliance.clearLatchedKeyPresses = function() {
+        appliance.send(0xb3, [2]);
+    };
+    
+    appliance.glassTouchErrors = appliance.command({
+        command: 0xb3,
+        readData: [3],
+        format: [
+            "keyStatus:UInt16",
+            "keyBitmap:Bytes@5"
+        ]
+    });
+    
+    appliance.leds = appliance.command({
+        command: 0xb4,
+        format: [
+            "upperOvenLedStatus:Bytes@13",
+            "lowerOvenLedStatus:Bytes@13"
+        ]
+    });
+    
+    appliance.buzzerTone = appliance.command({
+        command: 0xb5,
+        format: "UInt8"
+    });
+    
     return appliance;
 }
 
